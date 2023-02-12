af098aa7c55d8518fb2c2251c3da3eea3d71b9c2
Gulu Guluzade <mister.guluem25@gmail.com>
2023-02-12 08:09:09 +0300

    Calculator works

--- js/main.js
+++ js/main.js
@@ -1,5 +1,39 @@
-// const addition = (a, b) => {
-//     return a + b    
-// }
+// Get elements by className
+const items = document.querySelectorAll('.item');
+const entry = document.querySelector('.entry');
 
-// console.log(addition(5, 10))
+// Select items and write down they in the entry(input = value)
+items.forEach((el) => {
+    el.addEventListener('click', selectedItem)})
+
+function selectedItem () {
+    let getItem = this.innerHTML
+    entry.value += getItem
+}
+
+// Clear entry(input = '')
+const clear_btn = document.querySelector('.button-clear').addEventListener('click', clearEntry)
+
+function clearEntry () {
+    entry.value = ''
+}
+
+// Get result
+const result = document.querySelector('.button-result').addEventListener('click', getResult);
+
+function getResult () {    
+    try{
+        let value = entry.value
+
+        if (!value){
+            console.error('Please, fill in to the end.')
+            alert('Please, fill in to the end.')
+        }
+        else{
+            let res = eval(value)
+            entry.value = res
+        }
+    } catch(error){
+        alert('Invalid or unexpected token.')
+    }
+}
